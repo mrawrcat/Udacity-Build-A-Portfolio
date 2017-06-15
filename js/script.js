8247775a4683dd67905ac5189726d8b777eee90c
C410 <C410@AceLabs.lagcc>
2017-06-15 15:21:02 -0400

    got stuff from gfycat

--- js/script.js
+++ js/script.js
@@ -1,15 +1,15 @@
-//  var $main_header = $('#main-header');
+ var $main_header = $('#main-header');
 
-//  console.log($main_header);
+ // console.log($main_header);
 
-// $('.btn-info').click(function(){
-// 	$($main_header).html('I tricked you!');
-// 	$('#sub-header').html('There is no stuff');
-// }); 
+$('.btn-info').click(function(){
+	$($main_header).html('I tricked you!');
+	$('#sub-header').html('There is no stuff');
+}); 
 $('#random-gif-btn').click(function(){
-	$.get("http://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&rating=r", function(data){
+	$.get("https://api.gfycat.com/v1/gfycats/trending", function(data){
   	console.log(data);
-	var img = '<img src="' + data.data.image_url +'">'
+	var img = '<img src="' + data.gfycats[9].gifUrl +'">'
   	//console.log(img);
   	$('#random-gif-container').html(img);  	
 	});
@@ -17,3 +17,5 @@ $('#random-gif-btn').click(function(){
 // $.get("http://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag=american+psycho", function(data){
 //   console.log(data);
 // });
+//http://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&rating=r
+//var img = '<img src="' + data.data.fixed_height_downsampled_url +'">'
